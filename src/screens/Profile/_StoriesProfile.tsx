038a6b857390f7f1eb8d1a2b765c3dd59864be00
conghoan1211 <hoanpham12112003@gmail.com>
2024-12-16 01:13:28 +0700

    add actionProfile, bioprofile, mutualProfile, update _inforprofile, buttonbase...

---
+++ src/screens/Profile/_StoriesProfile.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import Story from "../../components/Stories/Story";
+
+function StoryProfile() {
+    return (
+        <View>
+            <Story />
+        </View>
+    );
+}
+
+export default StoryProfile;
